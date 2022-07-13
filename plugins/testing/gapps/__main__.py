eeb6c9dadbc716484aab5f49f0f1426b030ed070
fnixdev <luisgatn000@gmail.com>
2022-07-12 23:21:57 -0300

    @fnixdev

--- plugins/testing/gapps/__main__.py
+++ plugins/testing/gapps/__main__.py
@@ -167,14 +167,7 @@ if userge.has_bot:
             page2 = BeautifulSoup(url2.content, "lxml")
             name = page2.tbody.find_all("th", {'headers': 'files_name_h'})
             btn = []
-            gapps_btn = List[InlineKeyboardButton] = []
             for item in name:
                 nam = item.find("a")
-                gapps_btn.append(InlineKeyboardButton(text=nam.span.text, url=nam['href']))
-            btn += sublists(gapps_btn, width=2)
+                btn.append(InlineKeyboardButton(text=nam.span.text, url=nam['href']))
             await cq.edit_message_text(text=f"**Select your preferred nik version**", reply_markup=InlineKeyboardMarkup(btn))
-
-
-def sublists(input_list: List[Any], width: int = 3) -> List[List[Any]]:
-    """retuns a single list of multiple sublist of fixed width"""
-    return [input_list[x : x + width] for x in range(0, len(input_list), width)]

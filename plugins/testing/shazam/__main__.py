60ee5499595a2e6bfa22387b9df6ea2972357045
fnixdev <luisgatn000@gmail.com>
2022-07-24 22:14:29 -0300

    @fnixdev

--- plugins/testing/shazam/__main__.py
+++ plugins/testing/shazam/__main__.py
@@ -26,17 +26,25 @@ shazam = Shazam()
 async def whichi_song(message: Message):
     reply = message.reply_to_message
     if not reply.audio:
-        await message.err("Reply audio needed.")
+        await message.err("<code>Reply audio needed.</code>")
         return
-    await message.edit("Downloading audio..")
+    await message.edit("<code>downloading audio..</code>")
     file = await message.client.download_media(
                 message=message.reply_to_message,
                 file_name=config.Dynamic.DOWN_PATH
             )
     try:
+        await message.edit("<code>identifying music</code>")
         res = await shazam.recognize_song(file)
-        await message.edit(res)
     except Exception as e:
         await message.reply(e)
         os.remove(file)
-        return await message.err("Failed to get sound data.")
+        return await message.err("<code>Failed to get sound data.</code>")
+    song = res["track"]
+    out = f"<b>Song Recognised!\n\n{song['title']}</b>\n<i>- {song['subtitle']}</i>"
+    if song["images"]["coverart"]:
+        await message.delete()
+        await message.reply_photo(photo=song["images"]["coverart"], caption=out)
+    else:
+        await message.edit(out)
+    os.remove(file)

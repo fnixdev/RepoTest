0437ee343b049efb7402c9a19b8725919c32f68b
fnixdev <luisgatn000@gmail.com>
2022-07-25 21:47:47 -0300

    @fnixdev

--- plugins/testing/audio_tools/__main__.py
+++ plugins/testing/audio_tools/__main__.py
@@ -1,4 +1,4 @@
-## == Modules Userge by fnix
+# == Modules Userge by fnix
 #
 # = All copyrights to UsergeTeam
 #
@@ -7,6 +7,8 @@
 """ audio tools """
 
 import os
+
+from pyrogram.enums import MessageMediaType
 
 from userge import userge, Message, config
 from userge.utils import runcmd
@@ -20,22 +22,35 @@ from userge.utils import runcmd
 )
 async def extract_audio(message: Message):
     replied = message.reply_to_message
-    if not replied or not replied.video:
+    if not replied:
         await message.edit("<code>Reply video needed.</code>")
         return
-    await message.edit("<code>downloading video..</code>")
-    file = await message.client.download_media(
-                message=message.reply_to_message,
-                file_name=config.Dynamic.DOWN_PATH
+    if replied.media == MessageMediaType.VIDEO:
+        await message.edit("<code>downloading video..</code>")
+        file = await message.client.download_media(
+            message=replied.video.file_id,
+            file_name=config.Dynamic.DOWN_PATH
+        )
+        dur = replied.video.duration
+        out_file = file + ".mp3"
+        try:
+            await message.edit("<code>trying extract audio</code>")
+            cmd = f"ffmpeg -i {file} -vn -acodec copy {out_file}"
+            await runcmd(cmd)
+            os.remove(file)
+            await message.edit("<code>uploading audio...</code>")
+            await message.delete()
+            await message.client.send_audio(
+                message.chat.id,
+                audio=out_file,
+                caption="<b>Audio extracted by @HilzuUB</b>",
+                duration=dur
             )
-    out_file = file + ".aac"
-    try:
-        cmd = f"ffmpeg -i {file} -vn -acodec copy {out_file}"
-        await runcmd(cmd)
-        os.remove(file)
-        await message.client.send_audio(message.chat.id, audio=out_file, caption="<b>Audio extracted by @HilzuUB</b>")
-    except Exception:
-        os.remove(file)
-        await message.edit("<code>Fail.</code>")
+            os.remove(out_file)
+        except Exception:
+            os.remove(file)
+            await message.edit("<code>Fail.</code>")
+            return
+    else:
+        await message.edit("<code>Reply video needed.</code>")
         return
-

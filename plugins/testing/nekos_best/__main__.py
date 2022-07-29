e876e51b44292ab3b66d0126b4348d0434c37aba
fnixdev <luisgatn000@gmail.com>
2022-07-29 11:48:11 -0300

    init neko

---
+++ plugins/testing/nekos_best/__main__.py
@@ -0,0 +1,82 @@
+## == Modules Userge by fnix
+#
+# = All copyrights to UsergeTeam
+#
+# ==
+
+""" nekos module """
+
+import random
+
+from userge import userge, Message
+from userge.utils import get_response
+
+API = "https://nekos.best/api/v2/"
+CATEGORIES = [
+    "baka",
+    "bite",
+    "blush",
+    "bored",
+    "cry",
+    "cuddle",
+    "dance",
+    "facepalm",
+    "feed",
+    "happy",
+    "highfive",
+    "hug",
+    "kiss",
+    "laugh",
+    "neko",
+    "pat",
+    "poke",
+    "pout",
+    "shrug",
+    "slap",
+    "sleep",
+    "smile",
+    "smug",
+    "stare",
+    "think",
+    "thumbsup",
+    "tickle",
+    "wave",
+    "wink",
+    "kitsune",
+    "waifu",
+    "handhold",
+    "kick",
+    "punch",
+    "shoot",
+    "husbando",
+    "yeet",
+]
+
+
+@userge.on_cmd(
+    "neko",
+    about={
+        "header": "Get SFW stuff from nekos.best",
+        "usage": "{tr}neko\n{tr}neko [Choice]",
+        "Choice": CATEGORIES,
+    },
+)
+async def nekos_best(message: Message):
+    """ get sfw nekos """
+    choice = message.input_str
+    if choice:
+        lower_choice = str(choice).lower()
+        if lower_choice in CATEGORIES:
+            await send_neko(message, lower_choice)
+        else:
+            await message.err("<code>Invalid input..</code>")
+    else:
+        await send_neko(message, random.choice(CATEGORIES))
+
+
+async def send_neko(message: Message, choice: str):
+    try:
+        resp = await get_response.json(API+choice)
+    except Exception:
+        return await message.edit("request error")
+    print(resp)
